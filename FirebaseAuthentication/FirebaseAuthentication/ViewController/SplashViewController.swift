02272bade25fab7a1845fa59b23e76afe70f3784
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-06-22 21:00:48 +0200

    Home page if the user is logged in, login page if not

--- FirebaseAuthentication/FirebaseAuthentication/ViewController/SplashViewController.swift
+++ FirebaseAuthentication/FirebaseAuthentication/ViewController/SplashViewController.swift
@@ -1,12 +1,32 @@
  
 
 import UIKit
+import FirebaseAuth
 
 class SplashViewController: UIViewController {
 
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Auth.auth().addStateDidChangeListener { auth, user in
+            if user == nil {
+//                navigate to the login page
+                let vc = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "LoginViewController")
+                self.view.window?.rootViewController = vc
+            } else {
+//                navigate to the Home page
+                let vc = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "HomeViewController")
+                self.view.window?.rootViewController = vc
+
+            }
+        }
+        
+        
     }
 
 
