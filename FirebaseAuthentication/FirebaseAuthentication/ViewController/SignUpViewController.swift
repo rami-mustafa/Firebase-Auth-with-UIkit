001a3926ac99f0f4bf457b1451865ace906d791d
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-06-23 20:49:52 +0200

    Register

--- FirebaseAuthentication/FirebaseAuthentication/ViewController/SignUpViewController.swift
+++ FirebaseAuthentication/FirebaseAuthentication/ViewController/SignUpViewController.swift
@@ -1,24 +1,74 @@
  
 
 import UIKit
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+   
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var passwordConfirmationTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+    @IBAction func onSignUpTap(_ sender: Any) {
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        let passwordConfirm = passwordConfirmationTextField.text ?? ""
+        
+        if !email.isEmpty && !password.isEmpty && password == passwordConfirm   {
+//            Sign Up
+            
+            Auth.auth().createUser(withEmail: email, password: password) { auth, error in
+                
+                if error == nil {
+//                    Navigate Home
+                    self.navigateToHomePage()
+                }else {
+//                    Display an error message 
+                }
+            }
+        } else {
+//            Display an error message
+            presentEmptyErrorMessage()
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
+    
+    
+    @IBAction func onLoginTap(_ sender: Any) {
+        let vc = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "LoginViewController")
+        self.view.window?.rootViewController = vc
+    }
+    
+    
+    private func presentEmptyErrorMessage() {
+        
+        let alert = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert,animated: true)
+    }
+    
+    
+    private func presentSignUpErrorMessage() {
+        
+        let alert = UIAlertController(title: "Error", message: "Failed to create a new account. Please check credentials and try again.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert,animated: true)
+    }
+    
+    
 
+    private func navigateToHomePage() {
+        let vc = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "HomeViewController")
+        self.view.window?.rootViewController = vc
+    }
 }
