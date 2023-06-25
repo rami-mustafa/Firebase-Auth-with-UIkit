56b7d29aa7dd82d838b25ea1a682bbeb3d80bf41
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-06-25 21:10:03 +0200

    Home View Controller

--- FirebaseAuthentication/FirebaseAuthentication/ViewController/HomeViewController.swift
+++ FirebaseAuthentication/FirebaseAuthentication/ViewController/HomeViewController.swift
@@ -1,16 +1,30 @@
  
 
 import UIKit
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
+    
+    @IBOutlet weak var homeLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        homeLabel.text = "You are logged in with the following email: \(Auth.auth().currentUser?.email ?? "")"
 
      }
     
 
  
+    @IBAction func onSignOutButton(_ sender: Any) {
+        try? Auth.auth().signOut()
+        
+        let vc = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "LoginViewController")
+        self.view.window?.rootViewController = vc
+    }
     
 
 }
